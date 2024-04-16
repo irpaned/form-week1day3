b7c26ddb9e207e6a55faf1a3cb9328e210a61715
irpaned <muhammadirfan2823@gmail.com>
2024-04-16 21:01:58 +0800

    Memperbaiki bug

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,40 +1,3 @@
-// function submitHandler() {
-//     let projectName = document.getElementById("projectName").value;
-//     let startDate = document.getElementById("startDate").value;
-//     let endDate = document.getElementById("endDate").value;
-//     let description = document.getElementById("description").value;
-//     let file = document.getElementById("file").value;
-    
-
-//     if (projectName == "") {
-//         return alert ("Please enter your project name!")
-//     } else if (startDate == "") {
-//         return alert ("Please enter your Start Date!")
-//     } else if (endDate == "") {
-//         return alert ("Please enter your End Date!")
-//     } else if (description == "") {
-//         return alert ("Please enter your Description!")
-//     } else if (file == "") {
-//         return alert ("Please upload your image!")
-//     } 
-    
-    
-
-//     const data = {
-//         projectName, startDate, endDate, description, file
-//     }
-
-//     const yourEmail = 'muhammadirfan2823@gmail.com'
-    
-//     let a = document.createElement("a")
-//     a.href = `https://mail.google.com/mail?view=cm&fs=1&to=${yourEmail}&su=${projectName}&body=${description}`
-//     a.click();
-
-//     console.log(data)
-
-
-// }
-
 const dataProject = [];
 
 function addProject(event) {
@@ -44,11 +7,8 @@ function addProject(event) {
     let startDate = document.getElementById("startDate").value;
     let endDate = document.getElementById("endDate").value;
     let description = document.getElementById("description").value;
-    let image = document.getElementById("image").value
-    // let nodeJs = document.getElementById("nodeJs").value;
-    // let nextJs = document.getElementById("nextJs").value;
-    // let reactJs = document.getElementById("reactJs").value;
-    // let typeScript = document.getElementById("typeScript").value;
+    let image = document.getElementById("image").files[0];
+    let imageURL = URL.createObjectURL(image)
     
 
     if (projectName == "") {
@@ -59,7 +19,7 @@ function addProject(event) {
         return alert ("Please enter your End Date!")
     } else if (description == "") {
         return alert ("Please enter your Description!")
-    } else if (image == "") {
+    } else if (imageURL == "") {
         return alert ("Please upload your image!")
     } 
 
@@ -101,7 +61,7 @@ function addProject(event) {
             startDate: startDate,
             endDate: endDate,
             description: description,
-            image: image,
+            image: imageURL,
             duration: duration
 
         }
@@ -115,38 +75,62 @@ function addProject(event) {
 }
 
     function newData() {
-        document.getElementById("listProject").innerHTML = ""
+        document.getElementById("containerListProject").innerHTML = ""
 
         for(let i = 0; i < dataProject.length; i++) {
             const projectSaya = dataProject[i]
 
-            document.getElementById("listProject").innerHTML +=`
-            <div id="listProject" class="project">
-                <div class="foto">
-                    <img src="${projectSaya.image}" alt="ini foto project">
-                </div> 
-                <div class="judul">
-                    <h5>${projectSaya.projectName}</h5>
-                    <p>${projectSaya.startDate} - ${projectSaya.endDate}</p>
-                    <p>Durasi: ${projectSaya.duration}</p>
-                </div>
-                <div class="deskripsi">
-                    <p>${projectSaya.description}</p>
-                </div>
-
-                <div class="icon">
-                    <i class="fa-brands fa-google-play"></i>
-                    <i class="fa-brands fa-android"></i>
-                    <i class="fa-brands fa-java"></i>
-                </div>
-               
-                <div class="button">
-                    <button>edit</button>
-                    <button>delete</button>
+            document.getElementById("containerListProject").innerHTML +=`
+            <div class="containerListProject"> 
+    
+                <div id="listProject" class="project">
+    
+                    <div class="foto">
+                        <img src="${projectSaya.image}" alt="ini foto project">
+                    </div> 
+    
+                    <div class="judul">
+                        <h5>${projectSaya.projectName}</h5>
+                        <p>${projectSaya.startDate} - ${projectSaya.endDate}</p>
+                        <br>
+                        <p>${projectSaya.duration}</p>
+                    </div>
+    
+                    <div class="deskripsi">
+                        <p>${projectSaya.description}</p>
+                    </div>
+    
+                    <div class="icon">
+                        <i class="fa-brands fa-google-play"></i>
+                        <i class="fa-brands fa-android"></i>
+                        <i class="fa-brands fa-java"></i>
+                    </div>
+    
+                    <div class="button">
+                        <button>edit</button>
+                        <button>delete</button>
+                    </div>
                 </div>
             </div>
         `
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
